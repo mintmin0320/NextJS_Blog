7e9990e26f33fdf8bba82ea80267f7562bcd2c16
mintmin <mintmin0320@gmail.com>
2023-09-13 17:15:17 +0900

    fix: layout openGraph 이미지 경로 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
     description: siteMetadata.description,
     url: './',
     siteName: siteMetadata.title,
-    images: siteMetadata.socialBanner,
+    images: [siteMetadata.socialBanner],
     locale: 'en_US',
     type: 'website',
   },
