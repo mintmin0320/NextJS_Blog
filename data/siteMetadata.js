b8b1dd29c0eafb8e2d7a9598b482d234215adf6c
mintmin <mintmin0320@gmail.com>
2023-09-15 23:32:54 +0900

    feat: 글 쓰기 챌린지 회고

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -8,8 +8,8 @@ const siteMetadata = {
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: '/static/images/logo.png',
-  socialBanner: '',
+  siteLogo: '/static/images/hamin.PNG',
+  socialBanner: '/static/images/banner.jpeg',
   // socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'mintmin0320@gmail.com',
