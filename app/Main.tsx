ef3f2ff584a47443ee182f91be97b0dbb42e5df5
mintmin <mintmin0320@gmail.com>
2024-11-02 20:42:14 +0900

    feat: 태그 분류 버튼

--- app/Main.tsx
+++ app/Main.tsx
@@ -1,13 +1,7 @@
-import Image from '@/components/Image'
-import Link from '@/components/Link'
 import ScrollTopAndComment from '@/components/ScrollTopAndComment'
-import siteMetadata from '@/data/siteMetadata'
-import { formatDate } from 'pliny/utils/formatDate'
 
+import Posts from '@/components/Posts'
 import 'css/tailwind.css'
-
-// 메인 페이지 표시할 post 갯수 지정
-const MAX_DISPLAY = 12
 
 export default function Home({ posts }) {
   return (
@@ -16,84 +10,7 @@ export default function Home({ posts }) {
         <p>안녕하세요 👋 프론트엔드 개발자 박하민입니다.</p>
         <p className="mt-3">이 블로그는 배우고 느낀 점을 기록하는 공간입니다!</p>
       </div>
-      <div className="w-9/12 pt-16">
-        <div className="flex space-y-2  justify-between items-center mb-4">
-          <p className="text-3xl leading-7 text-gray-700 dark:text-gray-200 flex justify-center">
-            {siteMetadata.description}
-          </p>
-          {posts.length > MAX_DISPLAY && (
-            <div className="flex justify-center text-base font-medium leading-6">
-              <Link
-                href="/blog"
-                className="text-primary-500 hover:text-black dark:hover:text-white pr-3"
-                aria-label="All posts"
-              >
-                전체 보기
-              </Link>
-            </div>
-          )}
-        </div>
-        <ul className="divide-y divide-gray-200 dark:divide-gray-500 place-content-center grid grid-cols-[repeat(auto-fill,280px)] gap-y-[142px] gap-x-[22px] mo:grid-cols-1">
-          {!posts.length && 'No posts found.'}
-          {posts.slice(0, MAX_DISPLAY).map((post, index) => {
-            // const randomBoolean = Math.random() >= 0.6
-            // const isFirstOrLast = index % 4 === 0 || index % 4 === 3
-            const isEven = index % 2 === 0
-            const { slug, title, tags, images, date } = post
-
-            if (!tags.includes('왓에버 멘토링')) {
-              return (
-                <Link
-                  key={slug}
-                  href={`/blog/${slug}`}
-                  className={`rounded-[16px] w-[280px] transition-transform duration-300 hover:scale-105 mo:w-full ${
-                    isEven ? 'h-[258px]' : 'h-[215px]'
-                  }`}
-                >
-                  <li className="rounded-[16px] dark:bg-[#424242] h-full w-full mb-10">
-                    <Image
-                      src={images?.[0] ?? '/path/to/default/image.jpg'}
-                      alt="thumbnail"
-                      width={280}
-                      height={isEven ? 258 : 215}
-                      className={`rounded-[16px] border mo:w-full
-                    ${isEven ? 'h-[258px]' : 'h-[215px]'}`}
-                    />
-
-                    <article>
-                      <time
-                        className="font-[400] pt-2 text-[13px] flex justify-center"
-                        dateTime={date}
-                      >
-                        <p className="border border-x-0 border-t-0 border-b-[#000] dark:border-0 dark:border-b dark:border-b-[#fff] ">
-                          {formatDate(date, siteMetadata.locale)}
-                        </p>
-                      </time>
-                      <div className="space-y-2 xl:grid xl:grid-cols-3 xl:items-baseline xl:space-y-0 px-3 pt-2 pb-1">
-                        <div className="space-y-5 xl:col-span-3">
-                          <div className="space-y-6">
-                            <div>
-                              <h2 className="font-bold leading-8 tracking-tight w-full hover:text-primary-500">
-                                {title}
-                              </h2>
-
-                              {/* <div className="flex flex-wrap">
-                            {tags.map((tag) => (
-                              <Tag key={tag} text={tag} />
-                            ))}
-                          </div> */}
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </article>
-                  </li>
-                </Link>
-              )
-            }
-          })}
-        </ul>
-      </div>
+      <Posts posts={posts} />
       <ScrollTopAndComment />
     </div>
   )
---
+++ components/Posts.tsx
@@ -0,0 +1,99 @@
+'use client'
+
+import siteMetadata from '@/data/siteMetadata'
+import { formatDate } from 'pliny/utils/formatDate'
+import { useMemo, useState } from 'react'
+import Image from './Image'
+import Link from './Link'
+
+const MAX_DISPLAY = 12
+
+const Posts = ({ posts }) => {
+  const tagList = useMemo(() => {
+    const tagsSet = new Set(['전체'])
+    posts.forEach((post) => {
+      post.tags.forEach((tag) => tagsSet.add(tag))
+    })
+    return Array.from(tagsSet)
+  }, [posts])
+
+  const [selectedTag, setSelectedTag] = useState('전체')
+
+  const filteredPosts = useMemo(() => {
+    if (selectedTag === '전체') {
+      return posts
+    }
+    return posts.filter((post) => post.tags.includes(selectedTag))
+  }, [posts, selectedTag])
+
+  return (
+    <div className="w-9/12 pt-4 flex flex-col gap-[16px] mo:hidden">
+      <ul className="p-4 flex flex-wrap gap-2">
+        {tagList.map((tag) => (
+          <li key={tag}>
+            <button
+              type="button"
+              className={`rounded p-3 text-sm font-semibold py-3 cursor-pointer hover:bg-slate-100 ${
+                selectedTag === tag ? 'bg-blue-500 text-white' : 'bg-gray-200'
+              }`}
+              onClick={() => setSelectedTag(tag)}
+            >
+              {tag}
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      {/* Posts list */}
+      <ul className="divide-y divide-gray-200 dark:divide-gray-500 place-content-center grid grid-cols-[repeat(auto-fill,280px)] gap-y-[142px] gap-x-[22px] mo:grid-cols-1">
+        {!filteredPosts.length && 'No posts found.'}
+        {filteredPosts.slice(0, MAX_DISPLAY).map((post, index) => {
+          const isEven = index % 2 === 0
+          const { slug, title, tags, images, date } = post
+          return (
+            <Link
+              key={slug}
+              href={`/blog/${slug}`}
+              className={`rounded-[16px] w-[280px] transition-transform duration-300 hover:scale-105 mo:w-full ${
+                isEven ? 'h-[258px]' : 'h-[215px]'
+              }`}
+            >
+              <li className="rounded-[16px] dark:bg-[#424242] h-full w-full mb-10">
+                <Image
+                  src={images?.[0] ?? '/path/to/default/image.jpg'}
+                  alt="thumbnail"
+                  width={280}
+                  height={isEven ? 258 : 215}
+                  className={`rounded-[16px] border mo:w-full ${
+                    isEven ? 'h-[258px]' : 'h-[215px]'
+                  }`}
+                />
+
+                <article>
+                  <time className="font-[400] pt-2 text-[13px] flex justify-center" dateTime={date}>
+                    <p className="border border-x-0 border-t-0 border-b-[#000] dark:border-0 dark:border-b dark:border-b-[#fff] ">
+                      {formatDate(date, siteMetadata.locale)}
+                    </p>
+                  </time>
+                  <div className="space-y-2 xl:grid xl:grid-cols-3 xl:items-baseline xl:space-y-0 px-3 pt-2 pb-1">
+                    <div className="space-y-5 xl:col-span-3">
+                      <div className="space-y-6">
+                        <div>
+                          <h2 className="font-bold leading-8 tracking-tight w-full hover:text-primary-500">
+                            {title}
+                          </h2>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </article>
+              </li>
+            </Link>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default Posts
